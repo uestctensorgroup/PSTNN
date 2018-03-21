9d154fde81cdd4acc4a0c4dae389a07ceded3502
TaiXiangJiang <taixiangjiang@gmail.com>
2018-03-21 18:52:01 +0800

    Update readme.m

--- readme.m
+++ readme.m
@@ -2,8 +2,15 @@
 % This demo contains the implementation of the experiments  in: 
 % Tai-Xiang Jiang, Ting-Zhu Huang, Xi-Le Zhao, Liang-Jian Deng;
 % ''A novel nonconvex approach to recover the low-tuba-rank tensor data:
-% when t-SVD meets PSSV'' submitted to Applied Mathematical Modelling
-% (AMM).
+% when t-SVD meets PSSV'' submitted to Journal of Compuational and applied 
+% Mathematics (JCAM)
+% The preprint is available at https://arxiv.org/abs/1712.05870
+% if you use this code, please cite
+%      @article{jiang2017novel,
+%        title={A novel nonconvex approach to recover the low-tubal-rank tensor data: when t-SVD meets PSSV},
+%        author={Jiang, Tai-Xiang and Huang, Ting-Zhu and Zhao, Xi-Le and Deng, Liang-Jian},
+%        journal={arXiv preprint arXiv:1712.05870},
+%        year={2017}}
 % Contact: taixiangjiang@gmail.com
 % Date: 7th Feb. 2018
 
